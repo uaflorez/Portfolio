61686ed1e3b4e9f425d383d9bff22ce2fbbc918c
Uriel.Florez <urielflorez29@gmail.com>
2022-03-20 15:48:39 -0500

    terminado del curriculum

--- Curriculum/js/app.js
+++ Curriculum/js/app.js
@@ -1,10 +1,29 @@
-var doughnutData = [
-	{
-		value: 50,
-		color: "#1ABC9C"
-	},
-	{
-		value: 50,
-		color: "#ECF0F1"
-	}
-];
+$(document).ready(function () {
+  //Cambio de header
+  $(window).on("scroll", function () {
+    let posicion = $("#pr").offset();
+    if ($(window).scrollTop() >= posicion.top) {
+      $("header").removeClass("transparente");
+    } else {
+      $("header").addClass("transparente");
+    }
+  });
+  // adicionamiento del menu lateral
+  $("#burger").click(function () {
+    $('#burger').toggleClass('abierto');
+    $('.menu').toggleClass('abierto');
+    $('abierto').toggleClass('a');
+  });
+  //smooth scroll
+  $("a").on("click", function () {
+    if (this.hash !== "") {
+      let hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800
+      );
+    }
+  });
+});
