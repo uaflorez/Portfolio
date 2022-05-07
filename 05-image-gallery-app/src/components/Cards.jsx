973545ec34ddf38b53d8043ba2522dc21005081b
Uriel.Florez <urielflorez29@gmail.com>
2022-05-06 23:18:40 -0500

    adicion proyecto de galeria en React

---
+++ 05-image-gallery-app/src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImgMediaCard from './Card'
+
+const Cards = () => {
+  return (
+    <div>
+      <ImgMediaCard />
+    </div>
+  )
+}
+
+export default Cards;
---
+++ 05-image-gallery-app/src/components/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from './Cards';
+
+const Container = () => {
+  return (
+    <div>
+      <Container maxWidth="sm">
+      <Cards />
+      </Container>
+    </div>
+  )
+}
+
+export default Container
