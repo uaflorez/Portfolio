4483528cf569c5d91afe375859e7c39b594e30a4
Uriel.Florez <urielflorez29@gmail.com>
2022-05-24 17:34:43 -0500

    actualización hoja de vida

---
+++ 05-image-gallery-app/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+	return (
+		<div className="spinner-border text-info text-center" role="status">
+  			<span className="visually-hidden">Loading...</span>
+		</div>
+	  
+  )
+}
+
+export default Loading
